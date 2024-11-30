e55ac11f2fbe5a4503cf13a6824984c31afe1ebe
Miles <milesjpool@gmail.com>
2024-11-30 01:43:22 +0000

    Setup network rules.

--- google_cloud/_setup-instances.sh
+++ google_cloud/_setup-instances.sh
@@ -9,6 +9,7 @@ AVAILABILITY_ZONE="$LOCATION-a"
 gcloud compute instances create-with-container $INSTANCE \
     --project $PROJECT_ID \
     --container-image=$IMAGE \
+    --container-env="PORT=80" \
     --zone "$AVAILABILITY_ZONE" \
     --machine-type=e2-micro \
     --boot-disk-size 10GB \
@@ -18,7 +19,8 @@ gcloud compute instances create-with-container $INSTANCE \
     --maintenance-policy=TERMINATE \
     --provisioning-model=SPOT \
     --instance-termination-action=STOP \
-    --reservation-affinity=any
+    --tags http-server \
+    --reservation-affinity=any 
 
 gcloud compute instances add-iam-policy-binding $INSTANCE \
     --zone "$AVAILABILITY_ZONE" \
@@ -28,3 +30,22 @@ gcloud compute instances add-iam-policy-binding $INSTANCE \
 gcloud iam service-accounts add-iam-policy-binding $COMPUTE_SERVICE_ACCOUNT_EMAIL \
     --role=roles/iam.serviceAccountUser \
     --member="serviceAccount:$ACTIONS_SERVICE_ACCOUNT_EMAIL"
+
+gcloud compute firewall-rules create http-traffic \
+    --description="Ingress HTTP traffic" \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --action=ALLOW \
+    --rules=tcp:80 \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=http-server
+
+# gcloud compute addresses create $INSTANCE-address \
+#     --network-tier=STANDARD \
+#     --region=$LOCATION
+#
+# gcloud compute instances add-access-config $INSTANCE \
+#     --zone=$AVAILABILITY_ZONE \
+#     --network-tier=STANDARD \
+#     --address=x.x.x.x` # Address from the previous step
