6dac9f64dbef0ec0f497d9b372c42eae885c664a
Miles <milesjpool@gmail.com>
2024-12-30 03:07:40 +0000

    Use resty for e2e test.

--- cmd/e2e-tests/test_suites/web_api.go
+++ cmd/e2e-tests/test_suites/web_api.go
@@ -1,11 +1,12 @@
 package test_suites
 
 import (
-	"io"
 	"net/http"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/milesjpool/kahd/e2e-tests/internal"
 	"github.com/milesjpool/kahd/e2e-tests/internal/assertions"
+	"github.com/milesjpool/kahd/internal/model"
 )
 
 type WebApiTestSuite struct {
@@ -14,22 +15,22 @@ type WebApiTestSuite struct {
 }
 
 func (s *WebApiTestSuite) Run(t *internal.TestContext) {
-	httpClient := &http.Client{}
+	restClient := resty.New()
 
 	t.Run("it gets 404 for an unknown resource", func(t *internal.TestContext) {
-		resp, err := httpClient.Get(s.URL + "/unknown")
+		resp, err := restClient.R().Get(s.URL + "/unknown")
 
 		assertions.NoErr(t, err, "error making request: %v", err)
-		assertions.Equals(t, http.StatusNotFound, resp.StatusCode)
+		assertions.Equals(t, http.StatusNotFound, resp.StatusCode())
 	})
 
 	t.Run("it retrieves API status", func(t *internal.TestContext) {
-		resp, err := httpClient.Get(s.URL + "/status")
-		body, _ := io.ReadAll(resp.Body)
-		bodyStr := string(body)
+		status := &model.Status{}
+		resp, err := restClient.R().SetResult(status).Get(s.URL + "/status")
 
 		assertions.NoErr(t, err, "error making request: %v", err)
-		assertions.Equals(t, http.StatusOK, resp.StatusCode)
-		assertions.Equals(t, `{"database_connection":"healthy"}`, bodyStr)
+		assertions.Equals(t, http.StatusOK, resp.StatusCode())
+		assertions.Equals(t, "application/json", resp.Header().Get("Content-Type"))
+		assertions.Equals(t, "healthy", status.DatabaseConnection)
 	})
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-resty/resty/v2 v2.16.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
+	golang.org/x/net v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
